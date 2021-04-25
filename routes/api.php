0c2ad5c516cca99ffa823adf27e3cda6e712a3bc
Tuấn <ntuan9870@gmail.com>
2021-04-25 10:50:43 +0700

    Lịch sử giá

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::group(["namespace"=>"App\Http\Controllers"],function() {
     Route::any('removeFavoriteProduct','ProductController@removeFavoriteProduct');
     Route::any('getFavoriteProduct','ProductController@getFavoriteProduct');
     Route::any('getFavorite','ProductController@getFavorite');
+    Route::any('getHistoryPrice','ProductController@getHistoryPrice');
     
 
     Route::any('category/add','CategoryController@add');
